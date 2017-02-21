2a82cd4b98b77909f99cce98f48e27b98b1b587c
Chen Wang <wangchen@cs.grinnell.edu>
2017-02-21 13:36:16 -0600

    Reading Exercise 02-21

---
+++ 02-21/binary.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+1. x = 153 = 10011001
+2. We will have 00010011001
+3. On the ASCII Table, 153 represent 231	99	10011001	™	&#153;	&trade;	Trade mark sign
+4. Binary code for 153.
