cd99208b1cd6fa4249db7bf5469c87e89f585169
Chen Wang <wangchen@cs.grinnell.edu>
2017-02-24 13:16:31 -0600

    Reading Exercies

---
+++ 02-24/float.c
@@ -0,0 +1,7 @@
+We know the sign is + because the first digit is 0.
+From the next eight digit we know the power is 6 so we have 2^6.
+The next digit is 1, so it would be 2^6 * 1.(decimal part).
+The decimal part would be 1/2 + 1/8 + 1/16 + 1/128 = 0.6953125.
+So the answer is + 2^6 * 1.6953125 = 108.5.
+
+Done.
